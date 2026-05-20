9ee40be8bf6171f33663bf69bdb0ac364ebb78ea
iv6119396-del <iv6119396@gmail.com>
2026-05-20 20:48:01 +0300

    Основы JS

---
+++ otus-qajs-master/src/tale.js
@@ -0,0 +1,21 @@
+function kolobok(nameCharacter) {
+    if (nameCharacter === 'дедушка') {
+        return 'Я от дедушки ушёл';
+    } else if (nameCharacter === 'заяц') {
+         return 'Я от зайца ушёл';
+    } else (nameCharacter === 'лиса') {
+        return 'Меня съели';
+    }
+    return 'Неизвестный персонаж'
+}
+
+
+
+function newYear(namePerson) {
+    if (namePerson === 'Дед Мороз') {
+        return "Дед Мороз! Дед Мороз! Дед Мороз!";
+  } else if (namePerson === 'Снегурочка') {
+        return "Снегурочка! Снегурочка! Снегурочка!";
+  }
+  return "Неизвестный персонаж";
+}
